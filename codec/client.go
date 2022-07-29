bd0a8bf75c8fd234150af1e461e1a32d0aef6700
nameless <zyx9104@gmail.com>
2022-07-29 15:04:35 +0800

    添加ClientCodec和Client文件基础结构

--- codec/client.go
+++ codec/client.go
@@ -1,1 +1,42 @@
 package codec
+
+import (
+	"bufio"
+	"io"
+	"net/rpc"
+
+	"github.com/z-y-x233/zrpc/header"
+	"github.com/z-y-x233/zrpc/serialize"
+)
+
+type ClientCodec struct {
+	r    *bufio.Reader
+	w    *bufio.Writer
+	c    io.ReadWriteCloser
+	s    serialize.Serializer
+	resp *header.ResponseHeader
+}
+
+func (cc *ClientCodec) ReadResponseHeader(resp *rpc.Response) error {
+	// err := cc.req.Read(cc.r)
+	// if err != nil {
+	// 	return err
+	// }
+
+	return nil
+}
+
+func (cc *ClientCodec) ReadResponseBody(body interface{}) error {
+	return nil
+
+}
+
+func (cc *ClientCodec) WriteRequest(req *rpc.Request, body interface{}) error {
+	return nil
+
+}
+
+func (cc *ClientCodec) Close() error {
+	return cc.c.Close()
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/z-y-x233/zrpc
 
 go 1.15
+
+require gopkg.in/go-playground/assert.v1 v1.2.1
